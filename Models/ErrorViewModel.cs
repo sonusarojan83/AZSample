c5dff64c619ad256fd09775933cf50db79cb56c7
Sonu Sarojan <SonuS@Orioninc.com>
2020-12-20 18:42:20 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
