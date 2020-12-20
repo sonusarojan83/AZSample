5d40874bbd4a512bd5b7af8e578a9803ca183c59
Sonu Sarojan <Sonusarojan@outlook.com>
2020-12-20 18:44:33 -0500

    my comment

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace AZSample
 {
     public class Program
     {
+        //my comment
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
